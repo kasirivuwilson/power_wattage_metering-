234d020b2875aadc013ec46e13512297f7a121e9
kasirivuwilson <104931099+kasirivuwilson@users.noreply.github.com>
2022-06-28 20:48:34 +0300

    Add files via upload

---
+++ umeme.py
@@ -0,0 +1,33 @@
+#make a heading or welcome message.
+from turtle import pu
+
+
+print('\t umeme self help'.upper())
+
+#Request for an account no from the user.
+account_number = input('Account No: ')
+
+#Ask for the amount to pay from the user.
+amount_to_pay = int(input('Amount to pay: '))
+class wattage:
+     
+
+     
+     def wilson(self,amount):
+         self.amount = amount
+         amount_per_unit = 1100
+         units = amount/amount_per_unit
+         return round(units)
+         
+         
+#Generate a random token to correspond to the purchased wattage.
+
+#wattage(amount_to_pay) 
+
+
+#Display the token to the user.
+#Display the current user balance.
+#Display the purchased wattage and its cost.
+print("you purchased {} units".format(wattage().wilson(amount_to_pay)))
+
+ 
