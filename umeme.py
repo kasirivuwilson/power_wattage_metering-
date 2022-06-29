7f877adbd69f08192848d38e6bae691548788c83
Kasirivu Kigongo Wilson <kasirivuwilson@gmail.com>
2022-06-29 13:40:44 +0300

    Update umeme.py

--- umeme.py
+++ umeme.py
@@ -1,4 +1,5 @@
 #make a heading or welcome message.
+#from curses.ascii import isdigit
 from turtle import pu
 
 
@@ -8,18 +9,38 @@ print('\t umeme self help'.upper())
 account_number = input('Account No: ')
 
 #Ask for the amount to pay from the user.
-amount_to_pay = int(input('Amount to pay: '))
-class wattage:
-     
+try:
+    amount_to_pay = int(input('Amount to pay: '))
 
-     
-     def wilson(self,amount):
-         self.amount = amount
-         amount_per_unit = 1100
-         units = amount/amount_per_unit
-         return round(units)
-         
-         
+    class wattage:
+        def wilson(self,amount):
+            self.amount = amount
+            amount_per_unit = 1100
+            units = amount/amount_per_unit
+            return round(units)
+except:
+    while (1):
+        print('\n \t Enter amount in figures only!')
+        amount_to_pay = int(input('Amount to pay: '))
+        if (amount_to_pay.isdigit()):
+            amount_to_pay = int(input('Amount to pay: '))
+            class wattage:
+                def wilson(self,amount):
+                    self.amount = amount
+                    amount_per_unit = 1100
+                    units = amount/amount_per_unit
+                    return round(units)
+            break 
+
+        print("you purchased {} units".format(wattage().wilson(amount_to_pay)))
+class wattage:
+    def wilson(self,amount):
+        self.amount = amount
+        amount_per_unit = 1100
+        units = amount/amount_per_unit
+        return round(units)
+
+
 #Generate a random token to correspond to the purchased wattage.
 
 #wattage(amount_to_pay) 
@@ -30,4 +51,3 @@ class wattage:
 #Display the purchased wattage and its cost.
 print("you purchased {} units".format(wattage().wilson(amount_to_pay)))
 
- 
